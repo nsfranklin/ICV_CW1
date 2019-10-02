ff3842d9c749f51b8035ff8cb8e881e69254b8da
Nicholas Franklin <nickfranklin@rocketmail.com>
2019-10-03 00:39:39 +0100

    Calc Colour histogram functions

--- Matlab Coursework1/ICV_calc_colour_histogram.m
+++ Matlab Coursework1/ICV_calc_colour_histogram.m
@@ -1,16 +1,14 @@
 function histout = ICV_calc_colour_histogram(imgin)
-
 width = size(imgin,1);
 height = size(imgin,2);
 rgbHist = zeros(256,3);
 
-disp(rgbHist);
-disp(imgin(1,1,1));
-for x = 1:width
-    for y = 1:height
-        rgbHist(imgin(x,y,1),1) = 1; %1 + rgbHist(imgin(x,y,1),1);
-        rgbHist(imgin(x,y,2),2) = 1; %+ rgbHist(imgin(x,y,2),2);
-        rgbHist(imgin(x,y,3),3) = 1; %+ rgbHist(imgin(x,y,3),3);
+for x = 1:width-1
+    for y = 1:height-1
+        rgbHist(uint8(imgin(x,y,1)),1) = 1 + rgbHist(imgin(x,y,1),1);
+        rgbHist(uint8(imgin(x,y,2)),2) = 1 + rgbHist(imgin(x,y,2),2);
+        rgbHist(uint8(imgin(x,y,3)),3) = 1 + rgbHist(imgin(x,y,3),3);
+
     end
 end
 
--- Matlab Coursework1/Q3.m
+++ Matlab Coursework1/Q3.m
@@ -2,8 +2,10 @@ file = fullfile('Datasets','DatasetB.avi');
 vidObj = VideoReader(file); % Create the Video reader object
 vidFrames = read(vidObj); %reading video frames into Mat
 
-histo = ICV_calc_colour_histogram(vidFrames(:,:,:,1));
+histo = cell(size(vidFrames,4));
 
-disp(histo);
+for x = 1:size(vidFrames,4);
+    histo{x} = ICV_calc_colour_histogram(vidFrames(:,:,:,1));
+end
 
 
