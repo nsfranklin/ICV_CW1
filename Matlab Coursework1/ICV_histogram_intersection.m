551d1272a314f020c116f79f34fb757594005135
nsfranklin <nickfranklin@rocketmail.com>
2019-10-17 20:33:43 +0100

    Need to work out how intersection works.

--- Matlab Coursework1/ICV_histogram_intersection.m
+++ Matlab Coursework1/ICV_histogram_intersection.m
@@ -1,9 +1,28 @@
-function intersectout = ICV_histogram_intersection(histo1, histo2)
+function intersectionout = ICV_histogram_intersection(hist1, hist2)
 
-intersectionout = zeroes(255,3);
+intersectionout = 0;
 
 for x = 1:255
-   intersectionout(x,1) = ICV_abs(histo1(x,1) - histo2(x,1));
-   intersectionout(x,2) = ICV_abs(histo1(x,2) - histo2(x,2));
-   intersectionout(x,3) = ICV_abs(histo1(x,3) - histo2(x,3));
+    if hist1(x,1) < hist2(x,1)
+        intersectionout = intersectionout + hist1(x,1);
+    else
+        intersectionout = intersectionout + hist2(x,1);
+    end
+    
+    if hist1(x,2) < hist2(x,2)
+        intersectionout = intersectionout + hist1(x,2);
+    else
+        intersectionout = intersectionout + hist2(x,2);
+    end
+    
+    if hist1(x,3) < hist2(x,3)
+        intersectionout = intersectionout + hist1(x,3);
+    else
+        intersectionout = intersectionout + hist2(x,3);
+    end
 end
+
+for x = 1:256
+    histTotal = histTotal + hist2
+end
+intersectionout
--- Matlab Coursework1/Q3.m
+++ Matlab Coursework1/Q3.m
@@ -4,7 +4,7 @@ vidFrames = read(vidObj); %reading video frames into Mat
 
 histo = cell(size(vidFrames,4));
 for x = 1:size(vidFrames,4)-1
-    disp(x);
+    %disp(x);
     %disp(vidFrames(:,:,:,10));
     temp = ICV_calc_colour_histogram(vidFrames(:,:,:,x),x);
     if x == 100
@@ -15,19 +15,47 @@ for x = 1:size(vidFrames,4)-1
         imwrite(vidFrames(:,:,:,200),'frame200.jpg');
         temp200 = temp;
     end
-    %histo{x} = temp;
+    histo{x} = temp;
 end
 
 plot(temp100,'DisplayName','temp100');
-xlim([0,255]);
+xlim([1,256]);
 saveas(gcf, 'HistogramFrame100.jpg');
 
 plot(temp200,'DisplayName','temp200');
-xlim([0,255]);
+xlim([1,256]);
 saveas(gcf, 'HistogramFrame200.jpg');
 
-ICV_histogram_intersection();
+intersection88and166 = ICV_histogram_intersection(histo{88},histo{166});
+plot(intersection88and166,'DisplayName','IntersectionFrame88and166');
+xlim([1,256]);
+saveas(gcf, 'IntersectionFrame88and166.jpg');
 
+intersection88and166 = ICV_histogram_intersection(histo{88},histo{166}); %the intersection of frame 88 and 166
+plot(intersection88and166,'DisplayName','IntersectionFrame88and166');
+xlim([1,256]);
+saveas(gcf, 'IntersectionFrame88and166.jpg');
+
+intersection240and6 = ICV_histogram_intersection(histo{240},histo{6});
+plot(intersection240and6,'DisplayName','IntersectionFrame240and6');
+xlim([1,256]);
+saveas(gcf, 'IntersectionFrame240and6.jpg');
+
+plot(histo{88},'DisplayName','temp100');
+xlim([1,256]);
+saveas(gcf, 'HistogramFrame88.jpg');
+
+plot(histo{166},'DisplayName','temp100');
+xlim([1,256]);
+saveas(gcf, 'HistogramFrame166.jpg');
+
+plot(histo{240},'DisplayName','temp100');
+xlim([1,256]);
+saveas(gcf, 'HistogramFrame240.jpg');
+
+plot(histo{6},'DisplayName','temp100');
+xlim([1,256]);
+saveas(gcf, 'HistogramFrame6.jpg');
 
 
 
