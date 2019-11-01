76bb5901b254e7ab8df363aa5c551657d3a22cda
Nick <nickfranklin@rocketmail.com>
2019-11-01 15:12:03 +0000

    finished rotate mask

--- Matlab Coursework1/ICV_rotate_mask.m
+++ Matlab Coursework1/ICV_rotate_mask.m
@@ -1,4 +1,15 @@
-function maskOut = ICV_rotate_mask(mask)
+function maskOut = ICV_rotate_mask(mask) %all my masks are symetric I have included this for completeness 
 
 maskwidth = size(mask,1);
 maskheight = size(mask,2);
+
+output = size(maskwidth,maskheight);
+
+for i = 1:maskwidth
+    for j = 1:maskheight
+        output(i,j) = mask(maskwidth+1-i,maskheight+1-j);
+    end
+end
+
+disp(output);
+maskOut = output
