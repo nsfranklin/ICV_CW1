c478372c7ea05a65644425db7f972db58834833a
Nick <nickfranklin@rocketmail.com>
2019-11-01 14:59:53 +0000

    Updated the READ ME and ICV isMask

--- Matlab Coursework1/ICV_isMaskSymetric.m
+++ Matlab Coursework1/ICV_isMaskSymetric.m
@@ -1,13 +1,12 @@
 function boolOut = ICV_isMaskSymetric(mask)
 maskwidth = size(mask,1);
 maskheight = size(mask,2);
-
 boolOut = true;
 
-
-
 for i = 1:maskwidth
-    for j = 1:maskheight
-        if = 
+    for j = i:maskheight
+        if mask(i,j) ~= mask(j,i)
+            boolOut = false;
+        end 
     end
 end
