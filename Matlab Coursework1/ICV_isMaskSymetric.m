5c10dedc4ac4935fe9fa70270333ebbbe32ef2a7
Nicholas Franklin <nickfranklin@rocketmail.com>
2019-11-01 11:51:36 +0000

    added missing methods

--- Matlab Coursework1/ICV_isMaskSymetric.m
+++ Matlab Coursework1/ICV_isMaskSymetric.m
@@ -1,1 +1,13 @@
 function boolOut = ICV_isMaskSymetric(mask)
+maskwidth = size(mask,1);
+maskheight = size(mask,2);
+
+boolOut = true;
+
+
+
+for i = 1:maskwidth
+    for j = 1:maskheight
+        if = 
+    end
+end
--- Matlab Coursework1/ICV_rotate_mask.m
+++ Matlab Coursework1/ICV_rotate_mask.m
@@ -1,2 +1,4 @@
 function maskOut = ICV_rotate_mask(mask)
 
+maskwidth = size(mask,1);
+maskheight = size(mask,2);
